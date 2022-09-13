b6b6ca3b1f66005cd9869857567de6d9ec4e153e
Jacob MacDougall <jacobmacdougall01@gmail.com>
2022-09-13 17:55:53 -0400

    initialize project

---
+++ README.md
@@ -0,0 +1,3 @@
+# 649_scrap
+
+The purpose of these scripts is to download the 649 winning the the BCLC sebiste playnow.com
---
+++ playnow_scrap.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import pandas as pd
+from datetime import datetime, timedelta
+from pathlib import Path
+
+"""
+https://www.cbc.ca/news/canada/nova-scotia/atlantic-lottery-is-retiring-the-balls-1.51075980
+"""
+
+given_site = "https://www.playnow.com/lottery/lotto-649-winning-numbers/?date=24/8/1988"
+base_649_site = 'https://www.playnow.com/lottery/lotto-649-winning-numbers/?date='
+
+data_path = Path('data')
+draw_date =     datetime(year=1988,month=8,day=24)
+end_draw_date = datetime(year=1988,month=12,day=31)
+
+def main():   
+    
+    while draw_date < end_draw_date:
+        if draw_date.strftime("%A") in ["Saturday","Wednesday"]:
+            draw_site = base_649_site + draw_date.strftime('%d/%m/%Y')
+            
+            driver = webdriver.Chrome()
+            driver.get(draw_site)
+            driver.implicitly_wait(8)
+
+            draw_soup = BeautifulSoup(driver.page_source,'html.parser')
+
+            draw_fn_path = Path(data_path,"lotto-649-winning-numbers-"+draw_date.strftime('%d-%m-%Y')+".html")
+            
+            with open(draw_fn_path,'w',encoding='utf-8') as f:
+                f.write(draw_soup.prettify())
+    
+            driver.quit()
+           
+        draw_date += timedelta(days=1)
+
+if __name__ == "__main__":
+    main()
