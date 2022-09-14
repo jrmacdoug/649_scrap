b9804758e7b90803015094b7f3fa2d82372878f8
Jacob MacDougall <jacobmacdougall01@gmail.com>
2022-09-14 12:00:49 -0400

    renamed scripts

--- sql_select_winnings_tb.py
+++ sql_select_winnings_tb.py
@@ -6,11 +6,15 @@ cur = con.cursor()
 
 # [print(row) for row in cur.execute("SELECT * FROM winnings WHERE num1 is null ORDER BY year")]
 # [print(row) for row in cur.execute("SELECT * FROM winnings WHERE prize_6_6 is null ORDER BY year")]
-[print(row) for row in cur.execute("SELECT * FROM winnings WHERE prize_6_6 is not null ORDER BY year")]
+# [print(row) for row in cur.execute("SELECT * FROM winnings WHERE prize_6_6 is not null ORDER BY year")]
 
-cur.execute("""
-SELECT year,COUNT(num1) FROM winnings WHERE prize_6_6 is not null group BY year
+
+res = cur.execute("""
+SELECT * FROM winnings     
+    WHERE year = 1990
 """)
+[print(row) for row in res]
+
 
 con.commit()
 con.close()
